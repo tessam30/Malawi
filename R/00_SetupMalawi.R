fbeefbf1c19e21b5ba8592ce65849ae9e2d964d7
Tessam <tim.essam@gmail.com>
2018-12-20 22:15:22 -0500

    updates

---
+++ R/00_SetupMalawi.R
@@ -0,0 +1,14 @@
+# Purpose: Load LAPA data and clean up
+# Author: Tim Essam, Ph.D | USAID GeoCenter
+# Date: 2018_12_20
+# Audience: USAID Malawi
+
+
+# Load data and any packages needed ---------------------------------------
+
+datapath <- "Data"
+dhspath <- "DHS"
+excelpath <- "Excel"
+graphpath <- "Graph"
+
+
