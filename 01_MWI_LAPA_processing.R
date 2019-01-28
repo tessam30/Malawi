3efe94090ca3673d57bf40974b0d3c631b7efce6
Tessam <tim.essam@gmail.com>
2019-01-27 22:31:49 -0500

    marking out coe

--- 01_MWI_LAPA_processing.R
+++ 01_MWI_LAPA_processing.R
@@ -135,78 +135,78 @@ lapa_full_dist %>%
 
 # Discarded code due to data entry issues ---------------------------------
 
-
-df_raw <- read_excel(file.path(datapath, "LAPA District councils Results.xlsx"), 
-                     sheet = "TOTALS FOR ALL COUNCILS") %>% 
-  select(-X__1, 
-         -X__2,
-         scores_descrip = "DISTRICT COUNCIL PERFORMANCE ASSESSMENT BASELINE SCORES", 
-         everything()) %>% 
-  mutate(category = ifelse(str_detect(scores_descrip, "Key Performance Area"), 1, 0),
-         dist_tot = ifelse(str_detect(scores_descrip, "TOTAL DISTRICT SCORES"), 1, 0),
-         area = gsub("Key Performance Area", "", scores_descrip)) %>% 
-  
-  # Set up text flag to use as a fill down that will allow for creating rollups
-  mutate(score_category = ifelse(category == 1, area, NA_character_),
-         score_category= sub("\\S+", "", score_category)) %>%
-  fill(score_category) %>% 
-  select(-(Nkhatabay:Nsanje), everything()) %>% 
-  select(-X__1, -X__2) %>% 
-  
-  # Reshape long to spot check averages, totals, etc
-  gather(., key = "district", value = "lapa_score", Nkhatabay:Nsanje) %>%
-  mutate(df_names = ifelse(category == 1, "lapa_tot", "lapa_dist")) %>% 
-  left_join(., mwi_geo_df, by = c("district" = "lapa_dist")) %>% 
-  select(CID, district, everything()) %>% 
-  select(-(GID_0:HASC_1))
-
-# Set aside district totals for checking numbers
-dist_totals_df <- df_raw %>% filter(dist_tot == 1)
-
-df_raw %>% 
-  filter(dist_tot != 1) %>% 
-  split(., .$df_names) %>% 
-  list2env(., envir = .GlobalEnv)
-
-
-# Flag any lapa scores that are greater than 4 for any row-category
-# M'Mbelwa and Karonga are wrong so we can throw out all the aggregated data
-# as it will be incorrect as well
-lapa %>% select(district, area, district, lapa_score) %>% 
-  filter(lapa_score > 4)
-
-
-lapa <- lapa_dist %>% 
-  mutate(lapa_score_flag = ifelse(lapa_score > 4, 1, 0),
-         lapa_score_mod = case_when(
-           district == "Karonga" & lapa_score_flag == 1 ~ 2,
-           district == "M'Mbelwa" & lapa_score_flag == 1 ~ 
-         )) 
-
-%>% 
-  group_by(district, score_category) %>% 
-  mutate(categ_totals = sum(lapa_score, na.rm = TRUE)) %>% 
-  ungroup() %>% 
-  group_by(district) %>% 
-  mutate(district_score = sum(lapa_score, na.rm = TRUE)) %>% 
-  group_by(area) %>% 
-  mutate(individ_score_tot = sum(lapa_score, na.rm = TRUE)) %>% 
-  ungroup()
-
-# Take a look at LAPA scores by category, sorting from overall greatest to smallest
-lapa %>% 
-  mutate(district_sort = fct_reorder(district, district_score)) %>% 
-  ggplot(aes(x = district, y = categ_totals)) +
-  geom_col() +
-  coord_flip() +
-  facet_wrap(vars(score_category))
-
-
-
-
-
-s# Extract just totals
-lapa_totals <- df_raw %>% 
-  filter(category == 1)
-
-# Notes - for each scores the max value is four. There are 28 districts in the data
+# 
+# df_raw <- read_excel(file.path(datapath, "LAPA District councils Results.xlsx"), 
+#                      sheet = "TOTALS FOR ALL COUNCILS") %>% 
+#   select(-X__1, 
+#          -X__2,
+#          scores_descrip = "DISTRICT COUNCIL PERFORMANCE ASSESSMENT BASELINE SCORES", 
+#          everything()) %>% 
+#   mutate(category = ifelse(str_detect(scores_descrip, "Key Performance Area"), 1, 0),
+#          dist_tot = ifelse(str_detect(scores_descrip, "TOTAL DISTRICT SCORES"), 1, 0),
+#          area = gsub("Key Performance Area", "", scores_descrip)) %>% 
+#   
+#   # Set up text flag to use as a fill down that will allow for creating rollups
+#   mutate(score_category = ifelse(category == 1, area, NA_character_),
+#          score_category= sub("\\S+", "", score_category)) %>%
+#   fill(score_category) %>% 
+#   select(-(Nkhatabay:Nsanje), everything()) %>% 
+#   select(-X__1, -X__2) %>% 
+#   
+#   # Reshape long to spot check averages, totals, etc
+#   gather(., key = "district", value = "lapa_score", Nkhatabay:Nsanje) %>%
+#   mutate(df_names = ifelse(category == 1, "lapa_tot", "lapa_dist")) %>% 
+#   left_join(., mwi_geo_df, by = c("district" = "lapa_dist")) %>% 
+#   select(CID, district, everything()) %>% 
+#   select(-(GID_0:HASC_1))
+# 
+# # Set aside district totals for checking numbers
+# dist_totals_df <- df_raw %>% filter(dist_tot == 1)
+# 
+# df_raw %>% 
+#   filter(dist_tot != 1) %>% 
+#   split(., .$df_names) %>% 
+#   list2env(., envir = .GlobalEnv)
+# 
+# 
+# # Flag any lapa scores that are greater than 4 for any row-category
+# # M'Mbelwa and Karonga are wrong so we can throw out all the aggregated data
+# # as it will be incorrect as well
+# lapa %>% select(district, area, district, lapa_score) %>% 
+#   filter(lapa_score > 4)
+# 
+# 
+# lapa <- lapa_dist %>% 
+#   mutate(lapa_score_flag = ifelse(lapa_score > 4, 1, 0),
+#          lapa_score_mod = case_when(
+#            district == "Karonga" & lapa_score_flag == 1 ~ 2,
+#            district == "M'Mbelwa" & lapa_score_flag == 1 ~ 
+#          )) 
+# 
+# %>% 
+#   group_by(district, score_category) %>% 
+#   mutate(categ_totals = sum(lapa_score, na.rm = TRUE)) %>% 
+#   ungroup() %>% 
+#   group_by(district) %>% 
+#   mutate(district_score = sum(lapa_score, na.rm = TRUE)) %>% 
+#   group_by(area) %>% 
+#   mutate(individ_score_tot = sum(lapa_score, na.rm = TRUE)) %>% 
+#   ungroup()
+# 
+# # Take a look at LAPA scores by category, sorting from overall greatest to smallest
+# lapa %>% 
+#   mutate(district_sort = fct_reorder(district, district_score)) %>% 
+#   ggplot(aes(x = district, y = categ_totals)) +
+#   geom_col() +
+#   coord_flip() +
+#   facet_wrap(vars(score_category))
+# 
+# 
+# 
+# 
+# 
+# s# Extract just totals
+# lapa_totals <- df_raw %>% 
+#   filter(category == 1)
+# 
+# # Notes - for each scores the max value is four. There are 28 districts in the data
